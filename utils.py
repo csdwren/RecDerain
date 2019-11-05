8963af99b99cc382c03ccf2ec2342c4808dca723
shangwei5 <43960503+shangwei5@users.noreply.github.com>
2019-11-05 18:06:02 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -40,6 +40,49 @@ def is_image(img_name):
     else:
         return  False
 
+# TODO: two pixel shuffle functions to process the images
+def pixelshuffle(image, scale):
+    '''
+    Discription: Given an image, return a reversible sub-sampling
+    [Input]: Image ndarray float
+    [Return]: A mosic image of shuffled pixels
+    '''
+    if scale == 1:
+        return image
+    w, h, c = image.shape
+    mosaic = np.array([])
+    for ws in range(scale):
+        band = np.array([])
+        for hs in range(scale):
+            temp = image[ws::scale, hs::scale, :]  # get the sub-sampled image
+            band = np.concatenate((band, temp), axis=1) if band.size else temp
+        mosaic = np.concatenate((mosaic, band), axis=0) if mosaic.size else band
+    return mosaic
+
+
+def reverse_pixelshuffle(image, scale, fill=0, fill_image=0, ind=[0, 0]):
+    '''
+    Discription: Given a mosaic image of subsampling, recombine it to a full image
+    [Input]: Image
+    [Return]: Recombine it using different portions of pixels
+    '''
+    w, h, c = image.shape
+    real = np.zeros((w, h, c))  # real image
+    wf = 0
+    hf = 0
+    for ws in range(scale):
+        hf = 0
+        for hs in range(scale):
+            temp = real[ws::scale, hs::scale, :]
+            wc, hc, cc = temp.shape  # get the shpae of the current images
+            if fill == 1 and ws == ind[0] and hs == ind[1]:
+                real[ws::scale, hs::scale, :] = fill_image[wf:wf + wc, hf:hf + hc, :]
+            else:
+                real[ws::scale, hs::scale, :] = image[wf:wf + wc, hf:hf + hc, :]
+            hf = hf + hc
+        wf = wf + wc
+    return real
+
 
 def print_network(net):
     num_params = 0
