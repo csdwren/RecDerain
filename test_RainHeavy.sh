fb1c88c2c353c6a9cca9a928286a0385f9fa8842
shangwei5 <43960503+shangwei5@users.noreply.github.com>
2020-04-14 23:02:55 +0800

    revision

---
+++ test_RainHeavy.sh
@@ -0,0 +1,5 @@
+
+# BRN
+python test_BRN.py --logdir logs/RainHeavy/BRN --save_path results/RainHeavy/BRN --data_path datasets/RainHeavy/rain/
+
+
---
+++ test_RainLight.sh
@@ -0,0 +1,4 @@
+# BRN
+python test_BRN.py --logdir logs/RainLight/BRN --save_path results/RainLight/BRN --data_path datasets/RainLight/rain/
+
+
